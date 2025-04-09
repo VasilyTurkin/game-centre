2fa31cd4d154c4d51f7dd51ffe28cdd7b5f52dc1
Vasily Turkin <turkin1051994@gmail.com>
2025-04-09 18:46:01 +0300

    Добавил миграции Компьютеров и Заказов

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Example register directive Blade
+        Blade::directive('ifGuest', function () {
+            return "<?php if (auth()->guest()):?>";
+        });
     }
 }
