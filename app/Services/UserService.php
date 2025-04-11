a065cef35dc11d6dab94957e1e1eb31517817cb6
Vasily Turkin <turkin1051994@gmail.com>
2025-04-11 17:14:46 +0300

    Изменил UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,32 +5,40 @@ namespace App\Services;
 use App\Exception\UserCheckExistsException;
 use App\Models\User;
 use Exception;
+use Illuminate\Support\Facades\Log;
 
 class UserService
 {
-    private Database $database;
-
-    public function __construct(Database $db)
-    {
-        $this->database = $db;
-    }
-
     /**
+     * Создает нового пользователя
+     *
+     * @param array $userData
+     * @return User
      * @throws UserCheckExistsException
      * @throws Exception
      */
     public function createUser(array $userData): User
     {
-
-        $repository = new UserRepository($this->database);
-
-        $user = new User($userData);
         try {
-            $repository->saveUser($user);
+            if ((new User)->where('email', $userData['email'])->exists()) {
+                throw new UserCheckExistsException('Пользователь с таким email уже существует');
+            }
+
+            return (new User)->create([
+                'login' => $userData['login'],
+                'email' => $userData['email'],
+                'userName' => $userData['userName'] ?? null,
+                'password' => $userData['password'],
+                'deposit' => $userData['deposit'] ?? 0,
+            ]);
+
         } catch (UserCheckExistsException $e) {
+            Log::error('Ошибка создания пользователя: ' . $e->getMessage());
+            throw $e;
+
+        } catch (Exception $e) {
+            Log::error('Неожиданная ошибка при создании пользователя: ' . $e->getMessage());
             throw new Exception("Ошибка в создании пользователя: " . $e->getMessage());
         }
-
-        return $user;
     }
 }
